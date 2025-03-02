7bbbfe597774e70406a8caf8b077c56cf3b26d7a
Lasitha Bandara <lybandara@gmail.com>
2025-03-02 08:35:12 +0530

    refactor: Refactor main UI

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -6,18 +6,13 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Code Game</h1>
+      <p>A tool to help testing your algorithm/AI skills with simple games</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
