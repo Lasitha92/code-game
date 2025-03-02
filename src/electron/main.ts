9ac487119ad47ea0d0efa3c7ee77037e2ced0412
Lasitha Bandara <lybandara@gmail.com>
2025-03-02 08:34:32 +0530

    chore: Add support for hot reloading React changes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isProd } from "./util/config.js";
 
 const createWindow = () => {
   const mainWindow: BrowserWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/build/react/index.html"));
+  if (isProd()) {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/build/react/index.html"));
+  } else {
+    mainWindow.loadURL("http://localhost:5123");
+  }
 };
 
 app.whenReady().then(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,8 @@ export default defineConfig({
   build: {
     outDir: "build/react",
   },
+  server: {
+    port: 5123,
+    strictPort: true
+  }
 });
