95c7720d9f8b9e25b2dd26ff4935b017df82da62
Lasitha Bandara <lybandara@gmail.com>
2025-03-07 07:10:10 +0530

    refactor: Refactor Home page UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,41 +1,38 @@
 import { Button, Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import "./App.css";
-import TicTacToe from "./src/games/tic-tac-toe/TicTacToe";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  let navigate = useNavigate();
+
   const startTicTacToe = () => {
-    console.log("Going to Tic Tac Toe");
+    navigate("/tic-tac-toe");
   };
 
+  const startGo = () => {};
+
   return (
-    <>
-      <Container className="p-3 d-flex flex-column align-items-center justify-content-center min-vh-100">
-        <Row>
-          <h1>Code Game</h1>
-        </Row>
-        <Row>
-          <p>
-            A tool to help testing your algorithm/AI skills with simple games
-          </p>
-        </Row>
-        <Row>
-          <Col>
-            <Button className="game-button" onClick={() => startTicTacToe()}>
-              Tic Tac Toe
-            </Button>
-          </Col>
-          <Col>
-            <Button className="game-button" onClick={() => startTicTacToe()}>
-              Tic Tac Toe
-            </Button>
-          </Col>
-        </Row>
-        <Row>
-          <TicTacToe />
-        </Row>
-      </Container>
-    </>
+    <Container className="p-3 d-flex flex-column align-items-center justify-content-center min-vh-100">
+      <Row>
+        <h1>Code Game</h1>
+      </Row>
+      <Row>
+        <p>A tool to help testing your algorithm/AI skills with simple games</p>
+      </Row>
+      <Row className="p-3" style={{ width: "30em" }}>
+        <Col>
+          <Button className="w-100" onClick={() => startTicTacToe()}>
+            Tic Tac Toe
+          </Button>
+        </Col>
+        <Col>
+          <Button className="w-100" onClick={() => startGo()}>
+            Go
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
