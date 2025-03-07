3cf18c76d4f26db64c939cedd2a44f4cd309b7b8
Lasitha Bandara <lybandara@gmail.com>
2025-03-07 07:09:14 +0530

    feat: Add routing support

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TicTacToe from "./src/games/tic-tac-toe/TicTacToe.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="tic-tac-toe" element={<TicTacToe />}></Route>
+    </Routes>
+  </BrowserRouter>,
 );
