6902f5baa2a8c68f101aaec446f723cefdb44798
Lasitha Bandara <lybandara@gmail.com>
2025-03-08 10:08:19 +0530

    fix: Fix routing not working in executable

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,14 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import TicTacToe from "./src/games/tic-tac-toe/TicTacToe.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="tic-tac-toe" element={<TicTacToe />}></Route>
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
